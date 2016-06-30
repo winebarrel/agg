e137c8c43e739d582ebd2e1b9ee8dbdaf61c7147
winebarrel <sgwr_dts@yahoo.co.jp>
2016-07-01 01:25:13 +0900

    Bump up version [ci skip]

--- homebrew/agg.rb
+++ homebrew/agg.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Agg < Formula
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 
   homepage 'https://github.com/winebarrel/agg'
   url "https://github.com/winebarrel/agg/releases/download/v#{VERSION}/agg-v#{VERSION}-darwin-amd64.gz"

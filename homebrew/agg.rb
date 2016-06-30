de90c9d61626313d92e9209d3e5fadc5b4e0a12c
winebarrel <sgwr_dts@yahoo.co.jp>
2016-07-01 01:52:12 +0900

    Fix formula sha256 [ci skip]

--- homebrew/agg.rb
+++ homebrew/agg.rb
@@ -5,7 +5,7 @@ class Agg < Formula
 
   homepage 'https://github.com/winebarrel/agg'
   url "https://github.com/winebarrel/agg/releases/download/v#{VERSION}/agg-v#{VERSION}-darwin-amd64.gz"
-  sha256 'f28c5aefa9284c387b5d3868a9e004abd536340e63b5e0452939ac1bd803f1e9'
+  sha256 '7b04bef8173eac5bc4c36065d0020f2a4aabcc6a987f5ad5ace143bbed86de16'
   version VERSION
   head 'https://github.com/winebarrel/agg.git', :branch => 'master'
 
